f078291a2e81e6874d66b083c7ed5e051e33432f
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2020-05-27 21:15:38 +0200

    Dodati zadaci trece nedelje prakticnih vezbi.

---
+++ nedelja03/N03Z01.java
@@ -0,0 +1,143 @@
+/**
+ * Napisati program koji učitava n String-ova (n unosi korisnik), smeš-
+ * ta ih u jednostruko povezanu listu, i štampa sadržaj liste. Listu f-
+ * ormirati počevši od prazne liste, dodajući elemente s početka liste.
+ * Program potom:
+ * 
+ * a) štampa sve elemente liste koji su duzi od tri slova,
+ * b) utvrđuje broj pojava u listi elementa jednakog String-u b (kog u-
+ *    nosi korisnik),
+ * c) štampa poslednji element liste.
+ */
+
+class N03Z01 {
+	
+	public static void main(String[] args) {
+		
+		ListaStringova listaStringova = new ListaStringova();
+		
+		int n = Svetovid.in.readInt("Broj elemenata liste: ");
+		
+		for (int i = 0; i < n; i++) {
+			String input = Svetovid.in.readLine("Element #" + i + ": ");
+			listaStringova.dodajNaPocetak(input);
+		}
+		
+		System.out.println(listaStringova);
+		
+		listaStringova.stampajDuzeOdNSlova(3);
+		
+		String s = Svetovid.in.readLine("String za prebrojavanje: ");
+		System.out.println("String " + s + " se u listi javlja puta: " + listaStringova.brojPojava(s));
+		
+		listaStringova.stampajPoslednji();
+	}
+	
+}
+
+class ListaStringova {
+	
+	class Element {
+		
+		String info;
+		Element veza;
+		
+		
+		public Element(String info) {
+			this.info = info;
+			this.veza = null;
+		}
+		
+		
+		public String toString() {
+			return info;
+		}
+	}	// unutrasnja klasa
+	
+	
+	Element prvi;
+	int brojElemenata;
+	
+	
+	public ListaStringova() {
+		this.prvi = null;
+		this.brojElemenata = 0;
+	}
+	
+	
+	public void dodajNaPocetak(String info) {
+		
+		Element novi = new Element(info);
+		
+		novi.veza = prvi;
+		prvi = novi;
+		brojElemenata++;
+	}	// dodavanje novog elementa na pocetak
+	
+	
+	public void stampajDuzeOdNSlova(int n) {
+		
+		/** 
+		 * mislim da provera da li je lista prazna nije obavezna, doda-
+		 * o sam je radi udobnosti
+		 */
+		
+		if (prvi == null) {
+			System.out.println("Lista je prazna.");
+		} else {
+			
+			System.out.println("Elementi liste duzi od " + n + " elemenata: ");
+			
+			Element tekuci = prvi;
+			
+			while (tekuci != null) {
+				if (tekuci.info.length() > 3)
+					System.out.println(tekuci);
+				
+				tekuci = tekuci.veza;
+			}
+		}
+	}
+	
+	
+	public int brojPojava(String s) {
+		
+		int count = 0;
+		
+		Element tekuci = prvi;
+		
+		while (tekuci != null) {
+			if (tekuci.info.equals(s))
+				count++;
+			tekuci = tekuci.veza;
+		}
+		
+		return count;
+	}
+	
+	
+	public void stampajPoslednji() {
+		
+		Element tekuci = prvi;
+		
+		while (tekuci.veza != null) tekuci = tekuci.veza;
+		
+		System.out.println("Poslednji element liste: " + tekuci);
+	}
+	
+	
+	public String toString() {
+		
+		String output = "Lista [ ";
+		
+		Element tekuci = prvi;
+		
+		while (tekuci != null) {
+			output += tekuci + " ";
+			tekuci = tekuci.veza;
+		}
+		
+		return output + "]";
+	}
+
+}
---
+++ nedelja03/N03Z02.java
@@ -0,0 +1,216 @@
+/** 
+ * Napraviti klasu koja predstavlja jednostruko povezanu listu znakova
+ * (char). Koristiti najjednostavnije dodavanje na početak. Napisati p-
+ * rogram koji učitava n znakova i smešta ih u instancu napravljene kl-
+ * ase. U klasu dodati metode koji omogućavaju sledeće operacije i poz-
+ * vati ih iz glavnog programa:
+ * 
+ * a) štampanje samo znakova koji su velika slova
+ * b) izbacivanje svih znakova koji su mala slova
+ * c) izdvajanje u novu listu svih znakova koji su cifre. Rezultat - n-
+ * 	  ov objekat istog tipa lista, koristi se ista memorija za elemente
+ *    a elementi u novoj listi treba da su u istom redosledu kao u ori-
+ * 	  ginalnoj listi.
+ */
+
+class N03Z02 {
+	
+	public static void main(String[] args) {
+		ListaKaraktera listaKaraktera = new ListaKaraktera();
+		
+		String info;
+		int n;
+		
+		n = Svetovid.in.readInt("Broj elemenata liste: ");
+		for (int i = 0; i < n; i++) {
+			char c = Svetovid.in.readChar("Karakter #" + i + ": ");
+			listaKaraktera.dodajNaPocetak(c);
+		}
+		
+		System.out.println(listaKaraktera);
+		
+		listaKaraktera.stampajVelikaSlova();
+		
+		listaKaraktera.izbaciMalaSlova();
+		System.out.println(listaKaraktera);
+
+		ListaKaraktera listaCifara = listaKaraktera.izdvojCifre();
+		System.out.println(listaCifara);
+		
+	}
+}
+
+class ListaKaraktera {
+	
+	class Element {
+			
+		char info;
+		Element veza;
+		
+		
+		public Element(char info) {
+			this.info = info;
+			this.veza = null;
+		}
+		
+		
+		public String toString() {
+			return info + "";
+		}
+	}	// unutrasnja klasa
+	
+	
+	Element prvi;
+	int brojElemenata;
+	
+	
+	public void dodajNaPocetak(char info) {
+		
+		Element novi = new Element(info);
+		novi.veza = prvi;
+		prvi = novi;
+		brojElemenata++;
+	}
+	
+	
+	public void stampajVelikaSlova() {
+		
+		Element tekuci = prvi;
+		
+		System.out.print("Velika slova u listi: ");
+		
+		while (tekuci != null) {
+			if ('A' <= tekuci.info && tekuci.info <= 'Z')
+				System.out.print(tekuci + " ");
+			tekuci = tekuci.veza;
+		}
+		System.out.println();
+	}
+	
+	
+	public void izbaciMalaSlova() {
+		
+		while ('a' <= prvi.info && prvi.info <= 'z')
+			prvi = prvi.veza;
+		
+		if (prvi != null) {
+			Element prethodni = prvi;
+			
+			while (prethodni.veza != null) {
+				
+				if ('a' <= prethodni.veza.info && prethodni.veza.info <= 'z') {
+					prethodni.veza = prethodni.veza.veza;
+				} else {
+					prethodni = prethodni.veza;
+				}
+			}
+		}
+	}
+	
+	
+	public ListaKaraktera izdvojCifre() {
+		
+		ListaKaraktera cifre = new ListaKaraktera();
+		
+		Element cifreKraj = null;
+		
+		Element tekuci, prethodni;
+		
+		/**
+		 * prva provera - da li je na pocetku ulazne liste niz cifara
+		 */
+		
+		while (prvi != null && Character.isDigit(prvi.info)) {
+			
+			tekuci = prvi;
+			prvi = prvi.veza;
+			
+			if (cifre.prvi == null) {
+				
+				/**
+				 * na prvo mesto dolazi tekuci, kraj dolazi takodje is-
+				 * to na prvo mesto (buduci da postoji samo jedan elem-
+				 * ent u listi trenutno), poslednji element pokazuje na
+				 * null
+				 */
+				
+				cifre.prvi = tekuci;
+				cifreKraj = tekuci;
+				tekuci.veza = null;
+			} else {
+				
+				/**
+				 * ako je prvo mesto popunjeno kraj liste cifara pokaz-
+				 * ivace ka tekucem, tekuci ce pokazivati ka null a kr-
+				 * aj cifara ce se pomeriti na tekuci
+				 */
+				
+				cifreKraj.veza = tekuci;
+				tekuci.veza = null;
+				cifreKraj = tekuci;
+			}
+		}
+		
+		/**
+		 * proveravamo da li smo stigli do kraja ulazne liste i ako ni-
+		 * smo trazimo cifre dalje u listi
+		 */
+		
+		if (prvi != null) {
+			
+			tekuci = prvi;
+			
+			while (tekuci.veza != null) {
+				
+				prethodni = tekuci;
+				tekuci = tekuci.veza;
+				
+				if (Character.isDigit(tekuci.info)) {
+					
+					/**
+					 * ponovo imamo dve varijante gde su cifre ili pra-
+					 * zne ili nisu
+					 */
+					
+					prethodni.veza = tekuci.veza;
+					
+					if (cifre.prvi == null) {
+						
+						cifre.prvi = tekuci;
+						cifreKraj = tekuci;
+						tekuci.veza = null;
+					} else {
+						
+						cifreKraj.veza = tekuci;
+						tekuci.veza = null;
+						cifreKraj = tekuci;
+					}
+					
+					/**
+					 * bitan momenat - vracamo tekuci na prethodni zato
+					 * sto tako proveravamo da li imamo uzastopne cifre
+					 * u ulaznoj listi
+					 */
+					
+					tekuci = prethodni;
+				}
+			}
+		}
+		return cifre;
+	}	// izdvaja cifre iz originalne liste tako da se koristi ista memorija
+	
+	
+	public String toString() {
+		
+		String output = "Lista [ ";
+		
+		Element tekuci = prvi;
+		
+		while (tekuci != null) {
+			output += tekuci.info + " ";
+			tekuci = tekuci.veza;
+		}
+		
+		return output + "]";
+	}
+}
